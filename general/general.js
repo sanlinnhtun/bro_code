5d8fe1cc6e2b3abe0ff651569ee456d2657448eb
sanlinnhtun <lsanlynnh@gmail.com>
2024-11-11 01:42:42 +0630

    key enent

--- general/general.js
+++ general/general.js
@@ -1,188 +1,245 @@
-//!setting and getting in javascript
-class Person {
-  constructor(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-  }
+// //!setting and getting in javascript
+// class Person {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
 
-  set firstName(newFirstName) {
-    if (typeof newFirstName === 'string' && newFirstName.length > 0) {
-      this._firstName = newFirstName;
-    } else {
-      console.error('First name must be a non-epmty string');
+//   set firstName(newFirstName) {
+//     if (typeof newFirstName === 'string' && newFirstName.length > 0) {
+//       this._firstName = newFirstName;
+//     } else {
+//       console.error('First name must be a non-epmty string');
+//     }
+//   }
+
+//   set lastName(newLastName) {
+//     if (typeof newLastName === 'string' && newLastName.length > 0) {
+//       this._lastName = newLastName;
+//     } else {
+//       console.error('Last name must be a non-epmty string');
+//     }
+//   }
+
+//   set age(newAge) {
+//     if (typeof newAge === 'number' && newAge > 0) {
+//       this._age = newAge;
+//     } else {
+//       console.error('Age must be a greather than 0');
+//     }
+//   }
+
+//   get firstName() {
+//     return this._firstName;
+//   }
+//   get lastName() {
+//     return this._lastName;
+//   }
+//   get age() {
+//     return this._age;
+//   }
+
+//   get fullName() {
+//     return this._firstName + ' ' + this.lastName;
+//   }
+// }
+
+// const person = new Person('Bro', 'Code', 21);
+
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+// console.log(person.fullName);
+
+// //!Destructure
+
+// const person1 = {
+//   firstName: 'David',
+//   lastName: 'Hlaing',
+//   age: 22,
+//   job: 'Fry cook',
+// };
+
+// const person2 = {
+//   firstName: 'Jhon',
+//   lastName: 'Whick',
+//   age: 22,
+// };
+
+// // const { firstName, lastName, age, job } = person1;
+// const { firstName, lastName, age, job = 'Soilder' } = person2;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+
+// function personInfo({ firstName, lastName, age }) {
+//   console.log(`First name: ${firstName}`);
+//   console.log(`Last name: ${lastName}`);
+//   console.log(`Full name: ${firstName} ${lastName}`);
+//   console.log(`age: ${age}`);
+//   console.log(`job: ${job}`);
+// }
+
+// personInfo(person2);
+
+// const numbers = [10, 2, 3, 4, 5, 6, 7, 8, 9, 1];
+
+// numbers.sort((a, b) => b - a);
+
+// console.log(numbers);
+
+// const cards = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const random = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[random]] = [array[random], array[i]];
+//   }
+// }
+
+// shuffle(cards);
+
+// console.log(cards);
+
+// //!Closure
+
+// function createCounter() {
+//   let count = 0;
+
+//   function increamentCount() {
+//     count++;
+//     console.log(`Increament counter number is ${count}`);
+//   }
+
+//   function getCount() {
+//     return count;
+//   }
+
+//   return { increamentCount, getCount };
+// }
+
+// const counter = createCounter();
+
+// counter.increamentCount();
+// counter.increamentCount();
+// counter.increamentCount();
+// counter.increamentCount();
+// counter.increamentCount();
+// counter.increamentCount();
+
+// console.log(`The current count is ${counter.getCount()}`);
+
+// function createGame() {
+//   let score = 0;
+
+//   function increateFun(points) {
+//     score += points;
+//     console.log(`The score +${score} pst`);
+//   }
+
+//   function decreateFun(points) {
+//     score -= points;
+//     console.log(`The score -${score} pst`);
+//   }
+
+//   function getPoint() {
+//     return score;
+//   }
+
+//   return { increateFun, decreateFun, getPoint };
+// }
+
+// const game = createGame();
+
+// game.increateFun(10);
+// game.decreateFun(6);
+// // game.increateFun();
+
+// console.log(`The final score is ${game.getPoint()}`);
+
+// //! synchronous / Asynchronous
+
+// function func1(callback) {
+//   setTimeout(() => {
+//     console.log('Tast 1');
+//     callback();
+//   }, 3000);
+// }
+
+// function fucn2() {
+//   console.log('Tast 2');
+//   console.log('Tast 3');
+//   console.log('Tast 4');
+// }
+
+// func1(fucn2);
+
+// const btnTag = document.getElementById('btn');
+// const numTag = document.getElementById('num');
+
+// let num;
+
+// btnTag.onclick = function () {
+//   num = numTag.value;
+//   let real = Number(num);
+//   console.log(real);
+// };
+
+// //!DOM navigation
+
+// const myBox = document.getElementById('myBox');
+// const myButton = document.getElementById('btn');
+
+// myButton.addEventListener('click', (e) => {
+//   myBox.style.backgroundColor = 'tomato';
+//   myBox.textContent = 'OUCH 🥵';
+// });
+
+// myButton.addEventListener('mouseover', (e) => {
+//   myBox.style.backgroundColor = 'yellow';
+//   myBox.textContent = "Don't do that 😨";
+// });
+
+// myButton.addEventListener('mouseout', (e) => {
+//   myBox.style.backgroundColor = 'lightgreen';
+//   myBox.textContent = 'click me 😃';
+// });
+
+const myBox = document.getElementById('myBox');
+const moveMount = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener('keydown', (event) => {
+  myBox.textContent = '😣';
+  myBox.style.backgroundColor = 'tomato';
+});
+
+document.addEventListener('keyup', (event) => {
+  myBox.textContent = '😁';
+  myBox.style.backgroundColor = 'lightblue';
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key.startsWith('Arrow')) {
+    event.preventDefault();
+    switch (event.key) {
+      case 'ArrowUp':
+        y -= moveMount;
+        break;
+      case 'ArrowDown':
+        y += moveMount;
+        break;
+      case 'ArrowLeft':
+        x -= moveMount;
+        break;
+      case 'ArrowRight':
+        x += moveMount;
+        break;
     }
+    myBox.style.top = `${y}px`;
+    myBox.style.left = `${x}px`;
   }
-
-  set lastName(newLastName) {
-    if (typeof newLastName === 'string' && newLastName.length > 0) {
-      this._lastName = newLastName;
-    } else {
-      console.error('Last name must be a non-epmty string');
-    }
-  }
-
-  set age(newAge) {
-    if (typeof newAge === 'number' && newAge > 0) {
-      this._age = newAge;
-    } else {
-      console.error('Age must be a greather than 0');
-    }
-  }
-
-  get firstName() {
-    return this._firstName;
-  }
-  get lastName() {
-    return this._lastName;
-  }
-  get age() {
-    return this._age;
-  }
-
-  get fullName() {
-    return this._firstName + ' ' + this.lastName;
-  }
-}
-
-const person = new Person('Bro', 'Code', 21);
-
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.age);
-console.log(person.fullName);
-
-//!Destructure
-
-const person1 = {
-  firstName: 'David',
-  lastName: 'Hlaing',
-  age: 22,
-  job: 'Fry cook',
-};
-
-const person2 = {
-  firstName: 'Jhon',
-  lastName: 'Whick',
-  age: 22,
-};
-
-// const { firstName, lastName, age, job } = person1;
-const { firstName, lastName, age, job = 'Soilder' } = person2;
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(job);
-
-function personInfo({ firstName, lastName, age }) {
-  console.log(`First name: ${firstName}`);
-  console.log(`Last name: ${lastName}`);
-  console.log(`Full name: ${firstName} ${lastName}`);
-  console.log(`age: ${age}`);
-  console.log(`job: ${job}`);
-}
-
-personInfo(person2);
-
-const numbers = [10, 2, 3, 4, 5, 6, 7, 8, 9, 1];
-
-numbers.sort((a, b) => b - a);
-
-console.log(numbers);
-
-const cards = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const random = Math.floor(Math.random() * (i + 1));
-    [array[i], array[random]] = [array[random], array[i]];
-  }
-}
-
-shuffle(cards);
-
-console.log(cards);
-
-//!Closure
-
-function createCounter() {
-  let count = 0;
-
-  function increamentCount() {
-    count++;
-    console.log(`Increament counter number is ${count}`);
-  }
-
-  function getCount() {
-    return count;
-  }
-
-  return { increamentCount, getCount };
-}
-
-const counter = createCounter();
-
-counter.increamentCount();
-counter.increamentCount();
-counter.increamentCount();
-counter.increamentCount();
-counter.increamentCount();
-counter.increamentCount();
-
-console.log(`The current count is ${counter.getCount()}`);
-
-function createGame() {
-  let score = 0;
-
-  function increateFun(points) {
-    score += points;
-    console.log(`The score +${score} pst`);
-  }
-
-  function decreateFun(points) {
-    score -= points;
-    console.log(`The score -${score} pst`);
-  }
-
-  function getPoint() {
-    return score;
-  }
-
-  return { increateFun, decreateFun, getPoint };
-}
-
-const game = createGame();
-
-game.increateFun(10);
-game.decreateFun(6);
-// game.increateFun();
-
-console.log(`The final score is ${game.getPoint()}`);
-
-//! synchronous / Asynchronous
-
-function func1(callback) {
-  setTimeout(() => {
-    console.log('Tast 1');
-    callback();
-  }, 3000);
-}
-
-function fucn2() {
-  console.log('Tast 2');
-  console.log('Tast 3');
-  console.log('Tast 4');
-}
-
-func1(fucn2);
-
-const btnTag = document.getElementById('btn');
-const numTag = document.getElementById('num');
-
-let num;
-
-btnTag.onclick = function () {
-  num = numTag.value;
-  let real = Number(num);
-  console.log(real);
-};
+});
