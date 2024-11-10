29d61718907b1bcbb7fe93f7c9e1f6a348e2a4ef
sanlinnhtun <lsanlynnh@gmail.com>
2024-11-10 23:48:13 +0630

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+const display = document.getElementById('display');
+
+function appenToDisplay(input) {
+  display.value += input;
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+function clearDisplay() {
+  display.value = '';
+}
